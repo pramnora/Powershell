7e5ddb00fdd0e1f059df228fd67c9c30c647ed97
Paul Ramnora <pramnora@yahoo.com>
2023-02-04 16:11:11 +0000

    Update file01.ps1
    Corrected mis-spelled word: date->data/also, added a line below...DIR/-etc.

--- language/filing/file01.ps1
+++ language/filing/file01.ps1
@@ -1,7 +1,7 @@
 ni text01.txt       (Get-Alias ni -> New-Item/creates a new text file of specified: filename.filenameExtension)
 
-sc text01.txt abc   (Get-Alias sc -> Set-Content/Adds a line of text 'abc' into the file.
-                     NOTE: If the content contains spaces...; then, it's necessary to surround the date using a pair of quote marks: "a b c", instead.
+sc text01.txt abc   (Get-Alias sc -> Set-Content/Adds a line of text 'abc' into the named file.
+                     NOTE: If the content contains spaces...; then, it's necessary to surround the data using a pair of quote marks: "a b c", instead.
 
 ac text01.txt def   (Get-Alias ac -> Add-Content/Adds a line of text 'def' to the file...a line down below the previous content.)
 
@@ -9,3 +9,4 @@ gc text01.txt       (Get-Alias gc -> Get-Content/Displays on screen the contents
 
 clc text01.txt      (Get-Alias clc -> Clear-Content/Clears all of the text file contents...thus, leaving a file of 0 size in length)
                     
+dir                 (Get-Alias dir -> Get-ChildItem/Shows what is the current folder DIRectory listing/including the size of all files)
