5376a7a4d9999c4dfdfd0291aaf54572ddbb05f3
Paul Ramnora <pramnora@yahoo.com>
2023-02-04 16:53:59 +0000

    Create help01.ps1
    Showing different ways to get Powershell help.

---
+++ language/help/help01.ps1
@@ -0,0 +1,17 @@
+Get-Help
+
+Get-Help *command*
+...the asterix acts as a wildcard...meaning, find any cmdlet which contains either the exact name.../
+or, with other word prefixes written either before/or, after.
+
+Get-Command
+
+Get-Alias
+
+...all of the above are common ways of finding Powershell help.
+
+-----
+
+Yet, another way of finding help with Powershell commands is to open up the ISE/Integrated Script Editor...; 
+which when you type in commands...it, automatically, uses intellisense to complete the text.
+So, if you were to type in 'a'...you will see results such as: add/active/-etc.
