793b9e5743d28ce9f91548a5afe2168fbf780617
Paul Ramnora <pramnora@yahoo.com>
2023-02-04 04:02:07 +0000

    Update hw01.ps1
    Removed comma from description

--- language/hw/hw01.ps1
+++ language/hw/hw01.ps1
@@ -5,6 +5,6 @@ Echo "Hello, world!(4)"
 
 -----
 
-gal (Get-Alias, shows list of all Aliases)
-gal write (Get-Alias, write -> Write-Output)
-gal echo (Get-Alias, echo -> Write-Output)
+gal (Get-Alias...shows list of all Aliases)
+gal write (Get-Alias write -> Write-Output)
+gal echo (Get-Alias echo -> Write-Output)
