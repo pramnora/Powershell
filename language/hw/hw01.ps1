682b9496288dd5a2506e37c5c8f3c2c87ebf291c
Paul Ramnora <pramnora@yahoo.com>
2023-02-04 03:57:19 +0000

    Update hw01.ps1
    Extended description to include aliases

--- language/hw/hw01.ps1
+++ language/hw/hw01.ps1
@@ -2,3 +2,8 @@ Write-Output "Hello, world!(1)"
 Write-Host "Hello, world!(2)"
 Write "Hello, world!(3)"
 Echo "Hello, world!(4)"
+
+-----
+
+gal write (Get-Alias write -> Write-Output)
+gal echo (Get-Alias echo -> Write-Output)
